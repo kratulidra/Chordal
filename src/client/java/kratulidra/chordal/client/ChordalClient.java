ca4eace59f2347ef052012c73bccea62ec3fc7b4
Kratu <esteban9000000@gmail.com>
2026-09-09 08:45:02 -0500

    initial commit

---
+++ src/client/java/kratulidra/chordal/client/ChordalClient.java
@@ -0,0 +1,10 @@
+package kratulidra.chordal.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChordalClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
